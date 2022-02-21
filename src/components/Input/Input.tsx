2619475f4fac7f849f65bd96ed513678d8f75fd8
Чумак Сергей Викторович <svchumak1@mts.ru>
2022-02-22 02:22:53 +0300

    #26: Add profile page and validation

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,7 +11,7 @@ export function Input(props: IInputProps) {
     value,
     label,
     type,
-    autoComplete,
+    autoComplete = 'off',
     isValid = false,
     isInvalid = false,
     errorMessage = '',
